687a92b5f70cbaa7addee4da07d239377eaa72dd
Awad-Riad <151125049+Awad-Riad@users.noreply.github.com>
2025-08-20 20:46:38 +0300

    ..

--- src/Models/user.js
+++ src/Models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -16,11 +17,20 @@ const userSchema = new mongoose.Schema(
       required: true,
       lowercase: true,
       trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email Address");
+        }
+      },
     },
     password: {
       type: String,
       required: true,
-      minLength: 6,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Weak Password...Enter New Password");
+        }
+      },
     },
     age: {
       type: Number,
@@ -39,6 +49,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       default:
         "https://www.clipartmax.com/png/middle/202-2029196_shivaprakash-b-dummy-user.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid Photo Url");
+        }
+      },
     },
     about: {
       type: String,
@@ -46,6 +61,15 @@ const userSchema = new mongoose.Schema(
     },
     skills: {
       type: [String],
+      validate: {
+        validator: function (arr) {
+          if (arr.length > 5) {
+            throw new Error("array must have a 5 elemnt maximum");
+          }
+          return arr.length === new Set(arr).size; // check uniqueness
+        },
+        message: "Skills must be unique",
+      },
     },
   },
   { timestamps: true }
--- src/app.js
+++ src/app.js
@@ -1,18 +1,45 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const User = require("./Models/user");
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   //new instance from our user model
-  const user = new User(req.body);
   try {
+    const { firstName, lastName, emailId, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     await user.save();
-    res.send({ message: "User Added Successfully", user });
+    res.send({ message: "User Added Successfully", data: user });
   } catch (err) {
     res.status(400).send({ message: err.message });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      res.status(404).send({ message: "Invalid Credentials" });
+    }
+    const isPasswordVaid = await bcrypt.compare(password, user.password);
+    if (isPasswordVaid) {
+      res.send({ message: "User login Successfully", data: user });
+    } else {
+      res.status(404).send({ message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    res.status(400).send({ message: "Something Went Wrong:" + error.message });
   }
 });
 app.get("/user", async (req, res) => {
@@ -48,14 +75,23 @@ app.delete("/user", async (req, res) => {
   }
 });
 
-app.patch("/user", async (req, res) => {
-  const userId = req.body._id;
+app.patch("/user/:id", async (req, res) => {
+  const userId = req.params?.id;
   const data = req.body;
+  const allowedUpdates = ["photoUrl", "about", "gender", "age", "skills"];
+
   try {
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      allowedUpdates.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
       runValidators: true,
     });
+
     res.send({ message: "user updated successfully", user });
   } catch (error) {
     res.status(400).send({ message: "Something Went Wrong:" + error.message });
